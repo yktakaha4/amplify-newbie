a95ac1c77b53d5afe579c167ab4b0a6873ea70ce
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-02-10 09:48:50 +0900

    init

--- infra/terraform.tf
+++ infra/terraform.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "yktakaha4-tsukuro-iac"
     region = "ap-northeast-1"
-    key    = "tfstate/amplify-newbie/terraform.tfstate"
+    key    = "tf/amplify-newbie/terraform.tfstate"
   }
 }
