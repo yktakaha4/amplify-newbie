1ea1e1d5fda9dc6cf6adfd3e65cb617d564a18fb
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-02-10 09:58:24 +0900

    add amplify

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,8 @@ resource "aws_amplify_app" "amplify_newbie" {
   name       = "amplify-newbie"
   repository = "https://github.com/yktakaha4/amplify-newbie"
 
+  access_token = var.github_personal_access_token
+
   build_spec = file("${path.module}/build_spec.yml")
 
   custom_rule {
@@ -9,8 +11,16 @@ resource "aws_amplify_app" "amplify_newbie" {
     status = "404"
     target = "/index.html"
   }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.amplify_newbie.id
+  branch_name = "main"
+
+  framework = "React"
+  stage     = "PRODUCTION"
 
   environment_variables = {
-    ENV = "prd"
+    ENV = "production"
   }
 }
